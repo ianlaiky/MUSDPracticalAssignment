3ec80d4d43eda41ff5fba7b341935225f845bf46
Ian <152772a@mymail.nyp.edu.sg>
2016-12-05 23:49:05 +0800

    i

---
+++ app/src/main/java/com/example/ian/practicalassignment/CustomXchangeRate.java
@@ -0,0 +1,13 @@
+package com.example.ian.practicalassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomXchangeRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_xchange_rate);
+    }
+}
