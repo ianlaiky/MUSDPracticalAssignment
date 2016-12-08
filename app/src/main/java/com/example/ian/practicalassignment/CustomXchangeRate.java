c4d2182fe468598cb69c0be23e501d14c646f26b
Ian <152772a@mymail.nyp.edu.sg>
2016-12-08 19:19:22 +0800

    i

--- app/src/main/java/com/example/ian/practicalassignment/CustomXchangeRate.java
+++ app/src/main/java/com/example/ian/practicalassignment/CustomXchangeRate.java
@@ -2,12 +2,29 @@ package com.example.ian.practicalassignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 
 public class CustomXchangeRate extends AppCompatActivity {
+
+    Spinner spin;
+
+    ArrayAdapter<CharSequence>arr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_xchange_rate);
+
+
+        spin = (Spinner) findViewById(R.id.spin);
+        arr = ArrayAdapter.createFromResource(this, R.array.listCurr, android.R.layout.simple_spinner_dropdown_item);
+       spin.setAdapter(arr);
+
+
+
+
     }
 }
